70df0a0546fe92040b932374e71ac47e6b629748
therealmolf <miksbon@gmail.com>
2023-07-11 16:50:20 +0800

    Added Download CSV and Extract Table

--- scripts/download_bq_data.sh
+++ scripts/download_bq_data.sh
@@ -18,6 +18,7 @@ done
 
 PROJECT="artemis-1-391005"
 DATASET="agri"
+BUCKET="agri_csv_123"
 
 
 # Query USDA NASS Agri Dataset and Save Result to a Table
@@ -29,6 +30,17 @@ bq query --use_legacy_sql=false < $file_name --project_id $PROJECT --destination
 
 
 # Extract Table from BigQuery as CSV, then send it to Google Cloud Storage
+echo "-----------------------------------------------------------"
+echo "Sending BigQuery Table called " $destination_table " to" $destination_table ".csv as CSV"
+echo "-----------------------------------------------------------"
 
+bq extract --destination_format CSV $PROJECT:$DATASET.$destination_table gs://$BUCKET/$destination_table.csv
 
 # Download CSV to data dir from Google Cloud Storage
+echo "-----------------------------------------------------------"
+echo "Downloading " $destination_table ".csv to local"
+echo "-----------------------------------------------------------"
+
+gcloud storage cp gs://$BUCKET/$destination_table.csv /home/therealmolf/nass_agri_dash/data/agri/$destination_table.csv
+
+
